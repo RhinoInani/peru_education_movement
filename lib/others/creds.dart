5b91349a0c9fc12760e04f0c989e3b0f92c6b12b
RhinoInani <69125015+RhinoInani@users.noreply.github.com>
2022-03-19 09:49:14 -0700

    1.0.2. Updated with proper information.

---
+++ lib/others/creds.dart
@@ -0,0 +1,16 @@
+String credentials = r'''
+{
+  "type": "service_account",
+  "project_id": "perueducationmovement-37a67",
+  "private_key_id": "e5f99e37f35b2ce65f2b4bb62ff91e27648cb466",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDrqgyVtMTUyrnw\n1XJNZOdCbqYMa05iNzn2rEXjY7kcajnzenyfymgWfxkYYnc7DpYOln84g/rKHb3u\nxHKfXapFlqfWITgMyh0zmIwHBcwvYReieEbg3olYdNPWtqm0OUmqxI1pLewGvLSd\na6sdOPModDD/Yg0NB9DowEiTC3pHgSRdfY+emHokpHNZZOjfTJ1x1V+SYjTasc1y\ntxZ4lBIoa1Bup9RNlNF+YUFMYDJTZgukvLhtCqNBPuneUm6QHtXINE48lMx5EG0z\nBdhJQb6aaOBpMCu8vpT0PRdn4BthTT8LreTZRY/wx8g+/Khh5IF9WJNaiJOdAFOL\nAYNeVt4bAgMBAAECggEAc4ffeIrwzcL+ye7SBNgZOWumMsNuX17kpyXIc1PhnWkE\nNPVQ4EMpqRGw+0/WVNjvPoKQJuQoz+opwUKEUNVp0oiriiGp6TcyfqdjylOQyI9o\nN0mQsc/W9Er9yS6uXoCROUr3al9NbPYY822jqwVOy3++v4xRiphQ1bHiQlgnn6Ji\nmfcfeLXpCuzqxz7Wy7zgN/1qh/GlTkVvamYF5KpHbqUp0ApsW8HSjcUCuk/UjjuM\nlfJIIZaHJKB5NQazGH/Wca2ScEVyEQip5EoOpKNyIY97E7ClIY8sqMEf9fUshjuy\nJK6NmDFUM+Yae22f+xKlxEToGAtZ8sfxoF14MFQ3KQKBgQD4cmEBG3SIdGAW+Z0z\nV8h5891rClCvPBgGeQ5oCX/I3v9++bGAdT64LAeWHaKIGolhW5As4io7IXY/jkpp\nXXlvHeMwEI9Y6skCjIe+0KG+FQBZzi9O4vZuhgZMpnMrn3LHPMbLgND0Zpv2tOd3\nVh69O/LSE3pufc5ruTqdK/NoqQKBgQDy1C+RoAZ+Ccu8T8R5SF1Ic+VcmEINlhGr\nmTb1r6+5rhmg6ge9CIFlAk6jEpPXiSB3Y+datCI2bzhNZMz48gi6BLnG0u+auwqX\n68NIP/Gj2msEyRGQp7FJxe53/cMdWM832ZsugKqoy/LWFr/CZpU0968hIxN/42hr\nXFoKN8F3IwKBgFhjuQc/0VHKtXT3EWp2zKg2jovKe7IcWx/SacGxv3ijfZqbEv90\ntv+nCnvpxy1M45tzi/Or8wJegUdTRD4AN3fE1lL/UUB9J0502MaWSzTpG9rXdBVI\nwgvIOu2COm9GpvsjMEauaajJ6Kr46XCXfxAsB9Odgm1U7N9+YtAQFuUhAoGBAMGB\nRzrz29iac8lng3jmqrdu68rC5ngvK3ukR8C3NQBxdajedjY0TfCxSvr2devfMYkS\niq2kOFNKhAJgnlqPfVbHJ0gClb+m54Uy8lZj5mqdRoT0udOdXlEcsrDZ/p8nQ1IN\nTXVcQSLsC2KBlGQVHTetXNi6O/LAxFgxqurTjY8jAoGAMkAizmDrwWzpipYI47Sz\n9yWrYm2cZv5sqIQpO8RjL+1eL5oure1eWEw6XbuJSLfVxBjewJ+V5+r6Qv5qLn3r\nmb3b9vTjFdtfYNY4CFGF0Q/DQmClMvh3rp+4W43P1m+7GhHCaVnA489a3WtiC+lv\n+OCThHtliu3mjZmCNpz8G/U=\n-----END PRIVATE KEY-----\n",
+  "client_email": "perueducationmovement@perueducationmovement-37a67.iam.gserviceaccount.com",
+  "client_id": "103866442471175258715",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/perueducationmovement%40perueducationmovement-37a67.iam.gserviceaccount.com"
+}
+''';
+
+String spreadSheetId = "1EtHVE7A-lxovhOUSIccvzZD_LRnrMeM6z0f874vf8pc";
